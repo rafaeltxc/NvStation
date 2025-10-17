40839a482f8a52dbd274c8003445cc3c8acb6df0
Rafael Costa <rafael.txct@gmail.com>
2025-10-17 16:17:20 -0300

    fix/Removing Markdown plugin automatic build
    Plugin is built manually during setup due errors with its automation

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,6 @@
 local M = {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = function() vim.fn["mkdp#util#install"]() end,
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
   end,
