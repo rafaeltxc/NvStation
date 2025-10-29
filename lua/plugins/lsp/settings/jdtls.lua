76807e8badbc4a1629539b5d1e5c3886ca3edb18
Rafael Costa <rafael.txct@gmail.com>
2025-10-29 18:14:46 -0300

    feat/Cross-plataforma jdtls impl
    Final adjustment for cross-plataform config use, not only initialization scripts

--- lua/plugins/lsp/settings/jdtls.lua
+++ lua/plugins/lsp/settings/jdtls.lua
@@ -1,15 +1,32 @@
 local jdtls = require("jdtls")
 
+local system = vim.loop.os_uname().sysname
+local CONFIG
+if system == "Linux" then
+	CONFIG = "config_linux"
+elseif system == "Darwin" then
+	CONFIG = "config_mac"
+else
+	CONFIG = "config_win"
+end
+
+local HOME = vim.fn.expand("$HOME")
+local MASON = HOME .. "/.local/share/nvim/mason"
+local JDTLS_PATH = MASON .. "/packages/jdtls"
+local LOMBOK = MASON .. "/share/jdtls/lombok.jar"
+
+local launcher_path = vim.fn.glob(JDTLS_PATH .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+
 local bundles = {
 	vim.fn.glob("~/.local/share/nvim/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin.jar"),
 }
 
-vim.list_extend(bundles, vim.split(vim.fn.glob("~/.local/share/nvim/mason/share/java-test/*.jar", 1), "\n"))
+vim.list_extend(bundles, vim.split(vim.fn.glob(MASON .. "/share/java-test/*.jar", 1), "\n"))
 
 return {
 	on_attach = function(client, bufnr)
-    jdtls.setup_dap({ hotcodereplace = "auto" })
-    require("jdtls.dap").setup_dap_main_class_configs()
+		jdtls.setup_dap({ hotcodereplace = "auto" })
+		require("jdtls.dap").setup_dap_main_class_configs()
 	end,
 	cmd = {
 		"java",
@@ -24,16 +41,13 @@ return {
 		"java.base/java.util=ALL-UNNAMED",
 		"--add-opens",
 		"java.base/java.lang=ALL-UNNAMED",
-		"-javaagent:" .. vim.fn.expand("$HOME/.local/share/nvim/mason/share/jdtls/lombok.jar"),
-		-- "-agentlib:jdwp=transport=dt_socket,address=5005,server=y,suspend=n",
+		"-javaagent:" .. LOMBOK,
 		"-jar",
-		vim.fn.expand(
-			"$HOME/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar"
-		),
+		launcher_path,
 		"-configuration",
-		vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/config_linux"),
+		JDTLS_PATH .. "/" .. CONFIG,
 		"-data",
-		vim.fn.expand("$HOME/.cache/jdtls/workspace"),
+		HOME .. "/.cache/jdtls/workspace",
 	},
 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", "mvnw", ".git" }, { upward = true })[1]),
 	filetypes = { "java" },
@@ -47,25 +61,51 @@ return {
 				downloadSources = true,
 			},
 			configuration = {
-				runtimes = {
-					{
-						name = "JavaSE-1.8",
-						path = "/usr/lib/jvm/java-8-openjdk-amd64",
-					},
-					{
-						name = "JavaSE-11",
-						path = "/usr/lib/jvm/java-11-openjdk",
-					},
-					{
-						name = "JavaSE-17",
-						path = "/usr/lib/jvm/java-17-openjdk",
-					},
-					{
-						name = "JavaSE-21",
-						path = "/usr/lib/jvm/java-21-openjdk",
-						default = true,
-					},
-				},
+				runtimes = (function()
+					if system == "Darwin" then
+						return {
+							{
+								name = "JavaSE-1.8",
+								path = "/Library/Java/JavaVirtualMachines/openjdk-8.jdk/Contents/Home",
+							},
+							{
+								name = "JavaSE-11",
+								path = "/Library/Java/JavaVirtualMachines/openjdk-11.jdk/Contents/Home",
+							},
+							{
+								name = "JavaSE-17",
+								path = "/Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home",
+							},
+							{
+								name = "JavaSE-21",
+								path = "/Library/Java/JavaVirtualMachines/openjdk-21.jdk/Contents/Home",
+								default = true,
+							},
+						}
+					elseif system == "Linux" then
+						return {
+							{
+								name = "JavaSE-1.8",
+								path = "/usr/lib/jvm/java-8-openjdk-amd64",
+							},
+							{
+								name = "JavaSE-11",
+								path = "/usr/lib/jvm/java-11-openjdk",
+							},
+							{
+								name = "JavaSE-17",
+								path = "/usr/lib/jvm/java-17-openjdk",
+							},
+							{
+								name = "JavaSE-21",
+								path = "/usr/lib/jvm/java-21-openjdk",
+								default = true,
+							},
+						}
+					else
+						return {} -- Manual Windows configuration needed.
+					end
+				end)(),
 			},
 			project = {
 				referencedLibraries = {
