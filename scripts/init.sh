b2086c88bd294442e7b4951f95a2db9b4b240c5b
Rafael Costa <rafael.txct@gmail.com>
2025-10-29 18:12:55 -0300

    chore/Adjusting yarn check before plugin build
    Check command exitence before executing markdown plugin build

--- scripts/init.sh
+++ scripts/init.sh
@@ -15,28 +15,35 @@ LOMBOK_PATH="$HOME/.local/share/nvim/mason/share/jdtls/"
 LOMBOK_JAR="lombok.jar"
 
 if [ ! -f "$LOMBOK_PATH$LOMBOK_JAR" ]; then
-    curl -L -o "$LOMBOK_PATH$LOMBOK_JAR" https://projectlombok.org/downloads/lombok.jar
+  curl -L -o "$LOMBOK_PATH$LOMBOK_JAR" https://projectlombok.org/downloads/lombok.jar
 fi
 
 # Markdown plugin build
 MARKDOWN_PATH="$HOME/.local/share/nvim/lazy/markdown-preview.nvim/"
-
-pushd "$MARKDOWN_PATH" >/dev/null || exit 1
-yarn install
-popd >/dev/null
+if [ -d "$MARKDOWN_PATH" ]; then
+  if ! command -v yarn >/dev/null 2>&1; then
+    echo "'yarn' not found. Please install it manually."
+    echo "Markdown plugin build fail. Manual intervention needed."
+    wait 5
+  else
+    echo "Installing markdown-preview dependencies..."
+    pushd "$MARKDOWN_PATH" >/dev/null || exit 1
+    yarn install
+    popd >/dev/null
+  fi
+fi
 
 # NeotestJava setup
 NEOTEST_JAVA_PATH="$HOME/.local/share/nvim/neotest-java/"
 JUNIT_JAR="junit-platform-console-standalone-1.10.1.jar"
 
 if [ ! -f "$NEOTEST_JAVA_PATH$JUNIT_JAR" ]; then
-    mkdir -p "$NEOTEST_JAVA_PATH"
-    curl -L -o "$NEOTEST_JAVA_PATH$JUNIT_JAR" https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.10.1/junit-platform-console-standalone-1.10.1.jar
+  mkdir -p "$NEOTEST_JAVA_PATH"
+  curl -L -o "$NEOTEST_JAVA_PATH$JUNIT_JAR" https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.10.1/junit-platform-console-standalone-1.10.1.jar
 fi
 
 # Mark setup as done
-cat > "$PATH_POINTER" <<EOF
+cat >"$PATH_POINTER" <<EOF
 # DO NOT CHANGE
 POINTER=1
 EOF
-
